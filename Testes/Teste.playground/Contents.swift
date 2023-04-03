924eb69ef42babbe6e8898a2fcbd7921a2445040
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-04-03 17:50:50 -0300

    ++i;

--- Testes/Teste.playground/Contents.swift
+++ Testes/Teste.playground/Contents.swift
@@ -1,8 +1,21 @@
 import UIKit
 
-var idade:Int = 20
-var nome:String = "Teste"
-var peso:Float = 64.5
-var letra:Character = "C"
+var gasolina:Float = 5.15
+var alchool:Float = 3.15
 
-print("Hello World!!!")
+if((alchool / gasolina) < 0.70){
+    print("Abasteça com Alchool")
+}else{
+    print("Abasteça com Gasolina")
+}
+
+
+var preco:Float
+
+preco = alchool / gasolina
+
+if(preco < 0.70){
+    print("Abasteça com Alchool")
+}else{
+    print("Abasteça com Gasolina")
+}
