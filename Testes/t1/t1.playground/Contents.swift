a458e0f38a536a6e5fc0c6fe8f5887ef17a78ba2
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-04-04 14:12:12 -0300

    +=i

---
+++ Testes/t1/t1.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var gasolina:Float = 5.80
+var alcool:Float = 3.20
+
+if(alcool/gasolina) < 0.7{
+    print("Abasteça com Alcool")
+}else{
+    print("Abasteça com Gasolina")
+}
+    
