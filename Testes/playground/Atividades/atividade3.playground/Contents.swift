ae8256dd5545305572135ce0398887f5eaf56abb
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-04-12 11:01:22 -0300

    1

--- Testes/playground/Atividades/atividade3.playground/Contents.swift
+++ Testes/playground/Atividades/atividade3.playground/Contents.swift
@@ -1,11 +1,9 @@
 import Foundation
 
-
-
 var megaSena:[Int] = [19,8,23,17,5,43] // 0 e 60
 var lotoMania:[Int] = [19,81,90,67,54,6,34,2,45,1,4,76,8,9,24,3,67,89,54,35] // 0 e 99
 var lotoFacil:[Int] = [3,1,24,12,18,13,23,5,22,10,8,2,26,17,14] // 1 e 25
-var escolha:Int = 3
+var escolha:Int = 1
 var qtd:Int = 0
 var numeroAleatorio:[Int] = []
 var i:Int = 0
