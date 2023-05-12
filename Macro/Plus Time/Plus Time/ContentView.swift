aa22857c6fb1c42273222ed7481ef4333c8d8f0a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-05-12 14:15:19 -0300

    l

---
+++ Macro/Plus Time/Plus Time/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Plus Time
+//
+//  Created by Jairo Júnior on 12/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macro/Plus Time/Plus Time/Plus_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plus_TimeApp.swift
+//  Plus Time
+//
+//  Created by Jairo Júnior on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Plus_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
