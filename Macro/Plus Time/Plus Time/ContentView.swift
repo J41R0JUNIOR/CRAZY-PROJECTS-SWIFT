242fe57b22203ff0dbb3424d01c8902b4d91e43e
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-05-14 21:43:58 -0300

    Salvando pra nunca mais voltar (espero)

--- Macro/Plus Time/Plus Time/ContentView.swift
+++ Macro/Plus Time/Plus Time/ContentView.swift
@@ -11,15 +11,12 @@ var red = 0
 var green = 0
 var blue = 0
 
-
-
 //cores
 let cor1 = Color(red:93/255, green: 31/255, blue:221/255)
 let cor2 = Color(red:98/255, green: 84/255, blue:253/255)
 let cor3 = Color(red:238/255, green: 224/255, blue:255/255)
 
 let dark = Color(red:36/255, green: 36/255, blue:36/255)
-
 
 struct ContentView: View {
     
@@ -38,6 +35,37 @@ struct ContentView: View {
                     }
                 }.accentColor(.white)
             }
+        }
+    }
+}
+
+//Estruturas criadas pelo usuário para mostrar na tela inicial
+struct allStructures{
+    var title:String
+    var body:String
+}
+
+//vetor contendo as estruturas
+var TdsEstructs = [allStructures(title: "Teste", body: "Caso a pessoa digite mais do que eu defini, ela não consegue ultapassar. Digitando só pra testar a minha teoria")]
+
+//modal de add eventos/tarefas
+struct ModalView: View{
+    @Binding var estadoModal:Bool
+    var body: some View{
+        ZStack{
+            Color(red:36/255, green: 36/255, blue:36/255).ignoresSafeArea()
+            
+            VStack{
+                HStack{
+                    Button {
+                        estadoModal.toggle()
+                    } label: {
+                        Text("Done").foregroundColor(cor3)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }.padding()
         }
     }
 }
--- Macro/Plus Time/Plus Time/View1.swift
+++ Macro/Plus Time/Plus Time/View1.swift
@@ -9,20 +9,47 @@ import SwiftUI
  
 struct View1: View {
     var numViews:Int = 10
+    
+    // variavel pra abrir e fechar o modal
+    @State var estadoModal = false
+    
     var body: some View {
         ZStack {
             Color(red:98/255, green: 84/255, blue:253/255).ignoresSafeArea()
             
             //Color(cor2).ignoresSafeArea()
             VStack{
-                Text("+Time").foregroundColor(cor3).font(.system(size:40))
+                HStack{
+                    ZStack{
+                        //barra de pesquisa
+                        RoundedRectangle(cornerRadius: 10).frame(width: 308, height: 36).foregroundColor(cor3)
+                        HStack{
+                            Image(systemName: "magnifyingglass").foregroundColor(cor2)
+                            Text("Search").foregroundColor(cor2)
+                            Spacer()
+                        }.padding()
+                    }
+                    
+                    Spacer()
+                    
+                    //abrir modal de add eventos/tarefas
+                    Button {
+                        estadoModal.toggle()
+                    } label: {
+                        Image(systemName: "square.and.pencil").foregroundColor(cor3)
+                    }
+                    .sheet(isPresented: $estadoModal) {
+                        ModalView(estadoModal: $estadoModal)
+                    }
+                    
+                }.padding()
                 //Cor de fundo da ScrollView
                 ZStack{
                     RoundedRectangle(cornerRadius: 15).foregroundColor(dark).frame(width: 385)
                     
                     //ScrollView dos horários
                     ScrollView {
-                        ForEach (1..<numViews, id: \.self){ numero in
+                        ForEach (0..<numViews, id: \.self){ numero in
                             HStack{
                                 Button {
                                     
@@ -36,13 +63,13 @@ struct View1: View {
                                 
                                 VStack{
                                     HStack{
-                                        Text("Título").fontWeight(.bold).foregroundColor(.white).lineLimit(1)
+                                        Text(TdsEstructs[0].title).fontWeight(.bold).foregroundColor(.white).lineLimit(1)
                                             .allowsTightening(false)
                                         Spacer()
                                         
                                     }
                                     HStack{
-                                        Text("Caso a pessoa digite mais do que eu defini, ela não consegue ultapassar. Digitando só pra testar a minha teoria").font(.system(size: 15)).foregroundColor(cor3).lineLimit(2)
+                                        Text(TdsEstructs[0].body).font(.system(size: 15)).foregroundColor(cor3).lineLimit(2)
                                             .allowsTightening(false)
                                         Spacer()
                                         Text("20/02 >").fontWeight(.bold).foregroundColor(cor3)
