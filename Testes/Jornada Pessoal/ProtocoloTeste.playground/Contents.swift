c869660a867291b7436c9eba91f7775380f50b3a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-06-20 11:37:20 -0300

    __

--- Testes/Jornada Pessoal/ProtocoloTeste.playground/Contents.swift
+++ Testes/Jornada Pessoal/ProtocoloTeste.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+/*
+
 var greeting = "Hello, playground"
 
 protocol Animal {
@@ -21,3 +23,76 @@ struct Monkey: Animal{
     var species: String = "Sapajus apella"
     var canFly: Bool = false
 }
+*/
+
+
+/*
+print(teste)
+
+protocol jairo{
+    var inteligente: Bool { get }
+    var fera: Bool { get }
+}
+
+struct Caio: jairo{
+    var inteligente: Bool = true
+    var fera: Bool = false
+}
+*/
+
+/*
+class matador{
+    func matar(){
+        print("Fala do personagem:")
+    }
+}
+
+class Jason:matador{
+    var mascara:Bool
+    var motosserra:Bool
+    
+    init(mascara:Bool, motosserra:Bool){
+        self.mascara = mascara
+        self.motosserra = motosserra
+    }
+    
+    override func matar(){
+        if(mascara == true){
+            print("E o Jason com sua icônica máscara")
+        }else{
+            print("E o Jason sem sua icônica máscara")
+        }
+        
+        if(motosserra == true){
+            print("e com sua motosserra")
+        }else{
+            print("e sem sua motosserra")
+        }
+        print("Disse:")
+        print("Cuidado com a cabeça!")
+    }
+}
+
+
+var criador = Jason(mascara: false, motosserra: false)
+print(criador.matar())
+*/
+
+class bancoJr{
+    var conta:UUID
+    var saldo:Float
+    var nome:String
+    
+    init(conta:UUID, saldo:Float, nome:String){
+        self.conta = conta
+        self.saldo = saldo
+        self.nome = nome
+    }
+}
+
+class usuario: bancoJr{
+    override init(conta: UUID, saldo: Float, nome: String) {
+        super.init(conta: UUID(), saldo: Float(), nome: String())
+    }
+}
+
