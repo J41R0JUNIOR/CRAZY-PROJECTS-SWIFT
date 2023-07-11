ebc870a21a2e089e68d03d43466b6757604e0589
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-07-11 17:38:21 -0300

    .

---
+++ Testes/Projects/Jornada Pessoal/TestesLoucos/TestePulo/Teste Pulo/Teste Pulo/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  Teste Pulo
+//
+//  Created by Jairo Júnior on 09/07/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class JumpPlayer:GameScene{
+    
+    private var player: SKSpriteNode?
+    
+    override func sceneDidLoad() {
+        do {
+            player = SKSpriteNode(imageNamed: "player")
+            
+            let body = SKPhysicsBody(rectangleOf: player?.size ?? .zero)
+            body.affectedByGravity = true
+            body.allowsRotation = false
+            
+            player?.physicsBody = body
+            
+            if let playerNode = player{
+                self.addChild(playerNode)
+            }
+            
+            do {
+                let ground = SKSpriteNode(color: .brown, size: .init(width: 500, height: 100))
+                ground.position.y -= 200
+                
+                let body = SKPhysicsBody(rectangleOf: ground.size)
+                body.affectedByGravity = true
+                body.allowsRotation = false
+                body.isDynamic = false
+                
+                ground.physicsBody = body
+                
+                self.addChild(ground)
+                
+            }
+        }
+        
+        func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+            player?.physicsBody?.applyImpulse(.init(dx: 30, dy: 30))
+        }
+    }
+}
