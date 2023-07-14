8239c840a5964b8e32637b772cd55e15843eb4c7
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-07-14 14:29:36 -0300

    a

--- Testes/Projects/Jornada Pessoal/TestesLoucos/TestGame01/TestGame01/Scene/GameScene.swift
+++ Testes/Projects/Jornada Pessoal/TestesLoucos/TestGame01/TestGame01/Scene/GameScene.swift
@@ -14,13 +14,16 @@ class GameScene: SKScene {
     var moveLeftButton: SKSpriteNode!
     var moveRightButton: SKSpriteNode!
     var jumpButton: SKSpriteNode!
-    var doubleJumpButton: SKSpriteNode!
+
     var isMovingLeft = false
     var isMovingRight = false
     var cameraNode: SKCameraNode!
     
     var heroFrames = [SKTexture]()
     var textureAtlaas = SKTextureAtlas(named: "walk")
+
+    
+    
     
     
     override func didMove(to view: SKView) {
@@ -39,10 +42,6 @@ class GameScene: SKScene {
     
     func createMoveButtons() {
         
-        //jumpButton = childNode(withName: "botao") as? SKSpriteNode
-        //print("\(jumpButton)")
-        
-        
         jumpButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
         jumpButton.position = CGPoint(x: 1000, y: 10)
         
@@ -50,21 +49,20 @@ class GameScene: SKScene {
         jumpButtonlabel.fontColor = .white
         jumpButtonlabel.fontSize = 20
         jumpButtonlabel.position = CGPoint.zero
+        
         jumpButton.addChild(jumpButtonlabel)
-        
         addChild(jumpButton)
-        ///
-        ///
-        
         ///
         ///
         moveLeftButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
         moveLeftButton.position = CGPoint(x: -1000, y: 10)
         
+        
         let moveLeftLabel = SKLabelNode(text: "<-")
         moveLeftLabel.fontColor = .white
         moveLeftLabel.fontSize = 20
         moveLeftLabel.position = CGPoint.zero
+        
         moveLeftButton.addChild(moveLeftLabel)
         addChild(moveLeftButton)
         ///
@@ -76,10 +74,10 @@ class GameScene: SKScene {
         moveRightLabel.fontColor = .white
         moveRightLabel.fontSize = 20
         moveRightLabel.position = CGPoint.zero
-        moveRightButton.addChild(moveRightLabel)
         
+        moveRightButton.addChild(moveRightLabel)
         addChild(moveRightButton)
-        ///
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -137,14 +135,14 @@ class GameScene: SKScene {
         
         if let camera = cameraNode {
                 camera.position = character.position
-            }
+        }
                 
         // Atualize a posição dos botões de movimento para seguir o personagem
         moveLeftButton.position.x = character.position.x - 900
-        moveRightButton.position.x = character.position.x - 700
-        jumpButton.position.x = character.position.x  + 900
         moveLeftButton.position.y = character.position.y - 300
+        moveRightButton.position.x = character.position.x - 700
         moveRightButton.position.y = character.position.y - 300
+        jumpButton.position.x = character.position.x  + 900
         jumpButton.position.y = character.position.y - 300
         
     }
@@ -155,288 +153,13 @@ class GameScene: SKScene {
         
         if !moveLeftButton.contains(touchLocation) {
             isMovingLeft = false
+            character.removeAllActions()
         }
         
         if !moveRightButton.contains(touchLocation) {
             isMovingRight = false
+            character.removeAllActions()
         }
     }
 
 }
-
-/*
-class GameScene: SKScene {
-    var character: SKSpriteNode!
-    var moveLeftButton: SKSpriteNode!
-    var moveRightButton: SKSpriteNode!
-    var jumpButton: SKSpriteNode!
-    var doubleJumpButton: SKSpriteNode!
-    var isMovingLeft = false
-    var isMovingRight = false
-    var cameraNode: SKCameraNode!
-    
-    
-    override func didMove(to view: SKView) {
-        createMoveButtons()
-        
-        character = childNode(withName: "player") as? SKSpriteNode
-        cameraNode = SKCameraNode()
-        self.camera = cameraNode
-        addChild(cameraNode)
-    }
-    
-    func createMoveButtons() {
-        
-        //jumpButton = childNode(withName: "botao") as? SKSpriteNode
-        //print("\(jumpButton)")
-        
-        
-        jumpButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        jumpButton.position = CGPoint(x: 1000, y: 10)
-        
-        let jumpButtonlabel = SKLabelNode(text: "^")
-        jumpButtonlabel.fontColor = .white
-        jumpButtonlabel.fontSize = 20
-        jumpButtonlabel.position = CGPoint.zero
-        jumpButton.addChild(jumpButtonlabel)
-        
-        addChild(jumpButton)
-        ///
-        ///
-        
-        ///
-        ///
-        moveLeftButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        moveLeftButton.position = CGPoint(x: -1000, y: 10)
-        
-        let moveLeftLabel = SKLabelNode(text: "<-")
-        moveLeftLabel.fontColor = .white
-        moveLeftLabel.fontSize = 20
-        moveLeftLabel.position = CGPoint.zero
-        moveLeftButton.addChild(moveLeftLabel)
-        
-        addChild(moveLeftButton)
-        ///
-        ///
-        moveRightButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        moveRightButton.position = CGPoint(x: -800, y: 10)
-        
-        let moveRightLabel = SKLabelNode(text: "->")
-        moveRightLabel.fontColor = .white
-        moveRightLabel.fontSize = 20
-        moveRightLabel.position = CGPoint.zero
-        moveRightButton.addChild(moveRightLabel)
-        
-        addChild(moveRightButton)
-        ///
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = true
-            character.xScale = -1
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = true
-            character.xScale = 1
-        } else if jumpButton.contains(touchLocation) {
-            jumpCharacter()
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = false
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = false
-        }
-    }
-    
-    func jumpCharacter() {
-        character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 1000))
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        let characterSpeed: CGFloat = 5.0
-        
-        if isMovingLeft {
-            character.position.x -= characterSpeed
-        } else if isMovingRight {
-            character.position.x += characterSpeed
-        }
-        
-        if let camera = cameraNode {
-                camera.position = character.position
-            }
-                
-        // Atualize a posição dos botões de movimento para seguir o personagem
-        moveLeftButton.position.x = character.position.x - 900
-        moveRightButton.position.x = character.position.x - 700
-        jumpButton.position.x = character.position.x  + 900
-        moveLeftButton.position.y = character.position.y - 300
-        moveRightButton.position.y = character.position.y - 300
-        jumpButton.position.y = character.position.y - 300
-        
-    }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if !moveLeftButton.contains(touchLocation) {
-            isMovingLeft = false
-        }
-        
-        if !moveRightButton.contains(touchLocation) {
-            isMovingRight = false
-        }
-    }
-
-}
-*/
-
-
-/*
-class GameScene: SKScene {
-    var character: SKSpriteNode!
-    var moveLeftButton: SKSpriteNode!
-    var moveRightButton: SKSpriteNode!
-    var jumpButton: SKSpriteNode!
-    var doubleJumpButton: SKSpriteNode!
-    var isMovingLeft = false
-    var isMovingRight = false
-    var cameraNode: SKCameraNode!
-    
-    
-    override func didMove(to view: SKView) {
-        createMoveButtons()
-        
-        character = childNode(withName: "player") as? SKSpriteNode
-        cameraNode = SKCameraNode()
-        self.camera = cameraNode
-        character.addChild(cameraNode)
-    }
-    
-    func createMoveButtons() {
-        
-        //jumpButton = childNode(withName: "botao") as? SKSpriteNode
-        //print("\(jumpButton)")
-        
-        
-        jumpButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        jumpButton.position = CGPoint(x: 1000, y: 10)
-        
-        let jumpButtonlabel = SKLabelNode(text: "^")
-        jumpButtonlabel.fontColor = .white
-        jumpButtonlabel.fontSize = 20
-        jumpButtonlabel.position = CGPoint.zero
-        jumpButton.addChild(jumpButtonlabel)
-        
-        addChild(jumpButton)
-        ///
-        ///
-        
-        ///
-        ///
-        moveLeftButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        moveLeftButton.position = CGPoint(x: -1000, y: 10)
-        
-        let moveLeftLabel = SKLabelNode(text: "<-")
-        moveLeftLabel.fontColor = .white
-        moveLeftLabel.fontSize = 20
-        moveLeftLabel.position = CGPoint.zero
-        moveLeftButton.addChild(moveLeftLabel)
-        
-        addChild(moveLeftButton)
-        ///
-        ///
-        moveRightButton = SKSpriteNode(color: .gray, size: CGSize(width: 150, height: 150))
-        moveRightButton.position = CGPoint(x: -800, y: 10)
-        
-        let moveRightLabel = SKLabelNode(text: "->")
-        moveRightLabel.fontColor = .white
-        moveRightLabel.fontSize = 20
-        moveRightLabel.position = CGPoint.zero
-        moveRightButton.addChild(moveRightLabel)
-        
-        addChild(moveRightButton)
-        ///
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = true
-            character.xScale = -1
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = true
-            character.xScale = 1
-        } else if jumpButton.contains(touchLocation) {
-            jumpCharacter()
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = false
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = false
-        }
-    }
-    
-    func jumpCharacter() {
-        character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 1000))
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        let characterSpeed: CGFloat = 5.0
-        
-        if isMovingLeft {
-            character.position.x -= characterSpeed
-        } else if isMovingRight {
-            character.position.x += characterSpeed
-        }
-        
-       
-                
-        // Atualize a posição dos botões de movimento para seguir o personagem
-        moveLeftButton.position.x = character.position.x - 900
-        moveRightButton.position.x = character.position.x - 700
-        jumpButton.position.x = character.position.x  + 900
-        moveLeftButton.position.y = character.position.y - 300
-        moveRightButton.position.y = character.position.y - 300
-        jumpButton.position.y = character.position.y - 300
-        
-    }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        if !moveLeftButton.contains(touchLocation) {
-            isMovingLeft = false
-        }
-        
-        if !moveRightButton.contains(touchLocation) {
-            isMovingRight = false
-        }
-    }
-
-}
-*/
---
+++ Testes/Projects/Jornada Pessoal/TestesLoucos/TestGame01Pro/TestGame01Pro/Scenes/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  TestGame01Pro
+//
+//  Created by Jairo Júnior on 14/07/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+}
