292dd3b119e2ed1105a365a030db8d33200dd9e4
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-11-14 13:56:23 -0300

    ap

--- Testes/Projects/Jornada Pessoal/Chat/Chat/AppWithCloudKit/Notification/Notification.swift
+++ Testes/Projects/Jornada Pessoal/Chat/Chat/AppWithCloudKit/Notification/Notification.swift
@@ -7,8 +7,14 @@
 
 import SwiftUI
 import CloudKit
+import UserNotifications
 
 class PushNotificationCloudKit: ObservableObject{
+    
+    init(){
+        requestNotificationPermission()
+        subscribeToNotifications()
+    }
     
     func requestNotificationPermission(){
         let options: UNAuthorizationOptions = [.alert, .sound, .badge]
@@ -28,31 +34,29 @@ class PushNotificationCloudKit: ObservableObject{
         }
     }
     
-    func subscribeToNotifications(){
-        
+    func subscribeToNotifications() {
         let predicate = NSPredicate(value: true)
-        
-        let subscription = CKQuerySubscription(recordType: "Messages", predicate: predicate, subscriptionID: "message_added_to_database", options: [.firesOnRecordCreation])
-        
+        let subscription = CKQuerySubscription(recordType: "Messages", predicate: predicate, subscriptionID: "message_added_to_database", options: .firesOnRecordCreation)
+
         let notification = CKSubscription.NotificationInfo()
+//        notification.titleLocalizationKey = "%1$@"
+//        notification.titleLocalizationArgs = ["text"]
         notification.title = "New message"
-        notification.alertBody = "Come and see whats new"
+        notification.alertLocalizationKey = "%1$@"
+        notification.alertLocalizationArgs = ["text"]
         notification.soundName = "default"
-        
+
         subscription.notificationInfo = notification
-        
+
         CKContainer.default().publicCloudDatabase.save(subscription) { returnedSubscription, error in
-            if let error = error{
-                print(error)
-            }else{
+            if let error = error {
+                print("Error subscribing to notifications: \(error)")
+            } else {
                 print("Successfully subscribed to notifications")
             }
         }
     }
-    
     func unSubscribeToNotifications(){
-//        CKContainer.default().publicCloudDatabase.fetchAllSubscriptions(completionHandler: <#T##([CKSubscription]?, Error?) -> Void#>)
-        
         CKContainer.default().publicCloudDatabase.delete(withSubscriptionID: "message_added_to_database") { id, error in
             if let error = error{
                 print(error)
--- Testes/Projects/Jornada Pessoal/Chat/Chat/AppWithCloudKit/View/ContentView2.swift
+++ Testes/Projects/Jornada Pessoal/Chat/Chat/AppWithCloudKit/View/ContentView2.swift
@@ -10,40 +10,45 @@ import CloudKit
 
 
 struct ContentView2: View {
-    @StateObject var vm = viewModel()
+    @StateObject var vm = CloudKitSettings()
     @StateObject var not = PushNotificationCloudKit()
     
     var body: some View {
         VStack{
             HStack{
                 HStack{
-                    Button("Request"){
-                        not.requestNotificationPermission()
-                    }
-                    .buttonStyle(.borderedProminent)
-                    Button("Subscribe"){
-                        not.subscribeToNotifications()
-                    }
-                    .buttonStyle(.borderedProminent)
-                    Button("Ubsubscribe"){
-                        not.unSubscribeToNotifications()
-                    }
-                    .buttonStyle(.borderedProminent)
+//                    Button("Request"){
+//                        not.requestNotificationPermission()
+//                    }
+//                    .buttonStyle(.borderedProminent)
+//                    Button("Subscribe"){
+//                        not.subscribeToNotifications()
+//                    }
+//                    .buttonStyle(.borderedProminent)
+//                    Button("Ubsubscribe"){
+//                        not.unSubscribeToNotifications()
+//                    }
+//                    .buttonStyle(.borderedProminent)
                 }
             }
+            Divider()
+            List{
+                ForEach(vm.messages, id: \.self){item in
+                    Text(item.text)
+                        .onTapGesture {
+                            vm.updateItem(message: item)
+                        }
+                }.onDelete(perform:vm.deleteItem)
+            }.listStyle(.plain)
+            Divider()
             Spacer()
             TextField("Add message", text: $vm.text)
                 .textFieldStyle(.roundedBorder)
                 .onSubmit {
                     vm.addItem()
                     vm.text = ""
-                }
-            List{
-                ForEach(vm.messages, id: \.self){
-                    Text($0)
-                }
-            }.listStyle(.automatic)
-        }
+            }
+        }.padding()
         
     }
 }
