4d2fa3db39fe908ab15c69a2e6ad3cc9986d58ec
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-02-13 17:26:49 -0300

    foram pra outro repositorio

--- Projetos/Chat/Chat/AppWithCloudKit/Notification/NotificationSettings.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NotificationSettings.swift
-//  SKnano
-//
-//  Created by Jairo Júnior on 10/11/23.
-//
-
-import Foundation
--- Projetos/Chat/Chat/AppWithSwiftDataAndCloudKit/Data/CloudKit/CloudKitSetting.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CloudKitConfig.swift
-//  Chat
-//
-//  Created by Jairo Júnior on 09/11/23.
-//
-
-import Foundation
